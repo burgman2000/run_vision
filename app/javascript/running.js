5e5e94d20fc466f994fc5f461ba6691c06a157d3
unemekenta <unemekenta@gmail.com>
2023-12-09 19:29:04 +0900

    チャート表示まで

--- app/javascript/running.js
+++ app/javascript/running.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // 初期データを取得してグラフを描画
   updateChart();
 
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const ranDistance = document.getElementById("ran-distance").value;
-    
+
     // データをサーバーに送信
     const response = await fetch("/runnings", {
       method: "POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `running[ran_distance]=${ranDistance}`
+      body: `running[ran_distance]=${ranDistance}`,
     });
 
     if (response.ok) {
@@ -28,9 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function updateChart() {
   // サーバーからデータを取得
-  const response = await fetch("/runnings");
+  const response = await fetch("/runnings/json_index");
   const data = await response.json();
-
   // データから円グラフを描画
   drawChart(data);
 }
@@ -43,32 +42,34 @@ function drawChart(data) {
   const myChart = new Chart(ctx, {
     type: "pie",
     data: {
-      labels: data.labels,
-      datasets: [{
-        data: data.values,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
-          'rgba(54, 162, 235, 0.7)',
-          'rgba(255, 206, 86, 0.7)',
-          'rgba(75, 192, 192, 0.7)',
-          'rgba(153, 102, 255, 0.7)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1
-      }]
+      labels: data.months,
+      datasets: [
+        {
+          data: data.distances,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+            "rgba(255, 206, 86, 0.7)",
+            "rgba(75, 192, 192, 0.7)",
+            "rgba(153, 102, 255, 0.7)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       responsive: true,
       maintainAspectRatio: false,
       legend: {
-        position: 'right'
-      }
-    }
+        position: "right",
+      },
+    },
   });
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   
   root 'runnings#index'
   resources :runnings, only: [:index, :create]
+  get 'runnings/json_index', to: 'runnings#json_index'
 end
