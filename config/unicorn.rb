5c5f9bb761c44b2872f588032562590e096ecb82
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-12-13 17:46:04 +0900

    Unicornの設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -53,3 +53,4 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
