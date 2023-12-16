02f8c6e0f7cc9302822293bf3954f2785da08d06
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-12-16 11:59:01 +0900

    userカラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_11_111903) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_16_024835) do
   create_table "events", charset: "utf8", force: :cascade do |t|
     t.string "event_name", null: false
     t.integer "target_distance", null: false
@@ -24,6 +24,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_111903) do
     t.integer "ran_distance", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
