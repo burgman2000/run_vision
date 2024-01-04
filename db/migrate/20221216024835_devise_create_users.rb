6eaf582c47ed6c39c3dc99bb34517687f456a80c
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2024-01-04 17:24:21 +0900

    ニックネームでエラーが出る

--- db/migrate/20221216024835_devise_create_users.rb
+++ db/migrate/20221216024835_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname, null: false
+      t.string :nickname, null: true
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -32,18 +32,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_16_025932) do
   end
 
   create_table "runnings", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.integer "ran_distance", null: false
     t.text "ran_location", null: false
     t.text "impression", null: false
     t.date "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_runnings_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
+    t.string "nickname"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_16_025932) do
 
   add_foreign_key "comments", "runnings", on_update: :cascade, on_delete: :cascade
   add_foreign_key "comments", "users", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "runnings", "users", on_update: :cascade, on_delete: :cascade
 end
