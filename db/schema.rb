0911510afd90990a00634a3e607e18798ec16124
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2024-01-05 12:20:40 +0900

    nicknameのsanitizerメソッド

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_16_025932) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nickname"
+    t.string "nickname", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
