1201c62d8db049645f7c1de9b5d557a6409f14f4
unemekenta <unemekenta@gmail.com>
2024-01-20 00:08:38 +0900

    Fix: サポート

--- app/javascript/running.js
+++ app/javascript/running.js
@@ -8,14 +8,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const ranDistance = document.getElementById("ran-distance").value;
     const eventId = document.getElementById("event-id").value;
     // データをサーバーに送信
-    const response = await fetch("/runnings", {// response = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
+    const response = await fetch("/runnings", {
+      // response = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
       method: "POST",
       headers: {
         "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
           .content,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ running: { ran_distance: Number(ranDistance), event_id: Number(eventId)} }),
+      body: JSON.stringify({
+        running: {
+          ran_distance: Number(ranDistance),
+          event_id: Number(eventId),
+        },
+      }),
     });
 
     if (response.ok) {
@@ -39,11 +45,44 @@ async function updateChart() {
 let myChart;
 
 function drawChart(data) {
-  const ctx = document.getElementById("myChart").getContext("2d");// <canvas id="myChart"></canvas>
+  const ctx = document.getElementById("myChart").getContext("2d"); // <canvas id="myChart"></canvas>
+  var dataLabelPlugin = {
+    afterDatasetsDraw: function (chart, easing) {
+      var ctx = chart.ctx;
+      chart.data.datasets.forEach(function (dataset, i) {
+        var meta = chart.getDatasetMeta(i);
+        if (!meta.hidden) {
+          meta.data.forEach(function (element, index) {
+            ctx.fillStyle = "#333";
+
+            var fontSize = 14;
+            var fontStyle = "bold";
+            var fontFamily = "Helvetica Neue";
+            ctx.font = Chart.helpers.fontString(
+              fontSize,
+              fontStyle,
+              fontFamily
+            );
+
+            var dataString =
+              chart.data.labels[index] +
+              `\r\n` +
+              dataset.data[index].toString() +
+              "分";
+
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            var position = element.tooltipPosition();
+            ctx.fillText(dataString, position.x, position.y - fontSize / 2);
+          });
+        }
+      });
+    },
+  };
 
   if (myChart) {
     // すでにチャートが存在する場合は、データを更新する
-    myChart.data.labels = data.user_name;  //user_name//ラベル：月 // data = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
+    myChart.data.labels = data.user_name; //user_name//ラベル：月 // data = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
     console.log(data.user_name);
     myChart.data.datasets[0].data = data.by_user_distances; //by_user_distances//データ：距離
     console.log(data.by_user_distances);
@@ -55,10 +94,10 @@ function drawChart(data) {
     myChart = new Chart(ctx, {
       type: "pie",
       data: {
-        labels: data.user_name,// data = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
+        labels: data.user_name, // data = { "distances": [20, 30, 40, 50, 60], "months": ["2023-11", "2023-12", "2024-01", "2024-02", "2024-03"]}
         datasets: [
           {
-            data: data.by_user_distances,// [20, 30, 40, 50, 60]
+            data: data.by_user_distances, // [20, 30, 40, 50, 60]
             backgroundColor: [
               "rgba(255, 99, 132, 0.7)",
               "rgba(54, 162, 235, 0.7)",
@@ -77,7 +116,7 @@ function drawChart(data) {
           },
         ],
       },
-      // plugins: [ChartDataLabels],
+      plugins: [dataLabelPlugin],
       options: {
         responsive: true,
         maintainAspectRatio: false,
